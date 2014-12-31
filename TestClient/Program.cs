5f08509241faea4483779b6322d6497931e87d94
seager <seagerzhang@qq.com>
2014-12-31 13:45:57 +0800

    寒山闻钟html内容获取，尚未解决内容解析及添加进数据库

---
+++ TestClient/Program.cs
@@ -0,0 +1,13 @@
+﻿
+namespace TestClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new ClientHanShanWenZhong.Client();
+            client.Start();
+        
+        }
+    }
+}
